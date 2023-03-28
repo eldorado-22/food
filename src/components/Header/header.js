4a7de00f0856e3091ea1daa8f9ee3f224181f4cc
eldorado-22 <jumashevv992@gmail.com>
2023-03-28 22:05:13 +0600

    header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './header.scss';
+import {IoMenu} from "react-icons/io5";
+import user from './../../assets/img/users.png'
 const Header = () => {
     return (
-        <header>
-
+        <header id="header">
+            <div className="container">
+                <div className="header d-flex align-items-center justify-between">
+                    <IoMenu className="header--burger"/>
+                    <h1 className="text text-3xl text-black">Home</h1>
+                    <img src={user} alt="img"/>
+                </div>
+            </div>
         </header>
     );
 };
