d7b25cbcf31a1539c2c632bdd84772a998243135
eldorado-22 <jumashevv992@gmail.com>
2023-03-30 17:19:53 +0600

    nationalFoods File

---
+++ src/components/NationalFoods/National-Foods.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './National-Foods.scss';
+const NationalFoods = () => {
+    return (
+        <section>
+
+        </section>
+    );
+};
+
+export default NationalFoods;
