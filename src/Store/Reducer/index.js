4bdb2b5256eb7fdaa70a1b902cff0cb246e14be8
cerbent <kalykovadilet2004@gmail.com>
2023-04-04 13:56:47 +0600

    ordersBasket

---
+++ src/Store/Reducer/index.js
@@ -0,0 +1,17 @@
+const initialState = {
+    product: []
+}
+
+
+export const mainReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
+
+
+
+
+
--- src/pages/My-Order/My-Order.js
+++ src/pages/My-Order/My-Order.js
@@ -2,31 +2,20 @@ import React, {useState} from 'react';
 import sahar from "../../assets/img/image 46.png"
 import {TbCash} from "react-icons/tb";
 import {BsCreditCardFill, BsTerminalFill} from "react-icons/bs";
+import DetailBtn from "../detail-page/DetailBtn";
 const MyOrder = () => {
-    const [counter,setCounter] = useState(1)
-    const getMoney = () => {
-      if (counter > 1){
-          return setCounter(counter - 1)
-      }else {
-          return false
-      }
-    }
     return (
         <section id='orders'>
             <div className='container'>
                 <h1 className='flex justify-center text-xl'>cart</h1>
                 <div className='orders'>
                     <h1 className='my-4'>my order</h1>
-                    <div className='orders--block flex justify-between h-[68px] border-solid:ring-fuchsia-500'>
+                    <div className='orders--block flex justify-between items-center h-[68px] border-solid:ring-fuchsia-500'>
                             <div className='orders--block__image flex items-center'>
                                 <img className='w-[57px] mx-3' src={sahar} alt=""/>
                                 <h1>manti</h1>
                             </div>
-                            <div className='orders--block__state flex items-center mx-4'>
-                                <span className='cursor-pointer' onClick={getMoney}>-</span>
-                                <h1 className='mx-4'>{counter}x</h1>
-                                <button onClick={() => setCounter( counter + 1)}>+</button>
-                        </div>
+                          <DetailBtn/>
                     </div>
                 </div>
                 <div className='pay'>
