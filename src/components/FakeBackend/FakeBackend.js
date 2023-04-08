aadb40cfc81a11d13c1a8d63a075eb7f52c6076b
eldorado-22 <jumashevv992@gmail.com>
2023-04-08 21:22:34 +0600

    newUpdate

--- src/components/FakeBackend/FakeBackend.js
+++ src/components/FakeBackend/FakeBackend.js
@@ -29,13 +29,38 @@ export const kuurdak = [
         imageUrl: 'https://zor.store/images/detailed/40/Kuurdak_po-turecki_iz_baraniny.png',
     },
 ];
-//// NATIONAL FOODS //////
-
-
-/// DESERTS ////
-export const strawberry = [
+export const olobo = [
     {
         id: 4,
+        name: 'Olobo',
+        price: "250|350 som",
+        imageUrl: 'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkhPGimuudkcpBF7sKXt4WJKaKTM5SRkZCeTgDn6uOyic',
+    },
+];
+export const soup = [
+    {
+        id: 5,
+        name: 'Soup',
+        price: "200|290 som",
+        imageUrl: 'https://pakistanatlas.com/wp-content/uploads/2020/09/Pakistani-Food_0106_107-DuoDo-Soup.png',
+    },
+];
+export const lagman = [
+    {
+        id: 6,
+        name: 'Lagman',
+        price: "200|310 som",
+        imageUrl: 'https://lagman.one/img/lagman.png',
+    },
+];
+
+///// NATIONAL FOODS //////
+
+
+///// DESERTS //////
+export const strawberry = [
+    {
+        id: 7,
         name: 'Strawberry pie',
         price: "200|250 som",
         imageUrl: 'https://media.graphassets.com/resize=fit:clip,height:350,width:350/output=format:webp/tDr8BwFTSi74cSVtoZAW',
@@ -44,7 +69,7 @@ export const strawberry = [
 
 export const biscuit = [
     {
-        id: 5,
+        id: 8,
         name: 'Chokolate biscuit',
         price: "150|190 som",
         imageUrl: 'https://www.moonacoffee.com/wp-content/uploads/2015/02/slider_1.png',
@@ -53,10 +78,34 @@ export const biscuit = [
 
 export const ice = [
     {
-        id: 6,
+        id: 9,
         name: 'Chocolate ice cream',
         price: "190|240 som",
         imageUrl: 'https://images.ctfassets.net/l5fkpck1mwg3/4iQ8pziiW1pXkpVVBbSSWN/25d2de6298e197cd9220113567ff4b6c/Dessert_Loaded_Ice_Cream.png',
+    },
+];
+export const strawberryCake = [
+    {
+        id: 10,
+        name: 'Strawberry cake',
+        price: "900|1200 som",
+        imageUrl: 'https://az727718.vo.msecnd.net/04a723d76d5c4b8389637eb2e16b1272/images/8c404544378e4974835e818958be9b53@2x.png',
+    },
+];
+export const macarons = [
+    {
+        id: 11,
+        name: 'Macarons',
+        price: "1400|1500 som",
+        imageUrl: 'https://dufflet.com/wp-content/uploads/2020/11/hero_cat_SMACO_aug21.png',
+    },
+];
+export const sanSebastian = [
+    {
+        id: 12,
+        name: 'San Sebastian',
+        price: "340|390 som",
+        imageUrl: 'https://images.getbento.com/accounts/8e3ba4ef42a91981ef0c8c83d96b3e4a/media/images/57775RED_BERRY_OPERA_INDV._SWB.png?w=1200&fit=crop&auto=compress,format&crop=focalpoint&fp-x=0.5&fp-y=0.5',
     },
 ];
 /// DESERTS ////
@@ -65,16 +114,15 @@ export const ice = [
 //// PIZZA FOOD //////
 export const margarito = [
     {
-        id: 7,
+        id: 13,
         name: 'Margarito',
         price: "470|550 som",
         imageUrl: 'https://cicciopizza.ro/450-large_default/pizza-margherita.jpg',
     },
 ];
-
 export const pepperoni = [
     {
-        id: 8,
+        id: 14,
         name: 'Pepperoni',
         price: "500|650 som",
         imageUrl: 'https://kurgan.muka-ris.ru/upload/iblock/0bb/0bb567f02f21fbad4c2f24d1794601d3.png',
@@ -82,10 +130,34 @@ export const pepperoni = [
 ];
 export const italian = [
     {
-        id: 9,
+        id: 15,
         name: 'Italian',
         price: "535|670 som",
         imageUrl: 'https://francospizzaandpasta.co.uk/wp-content/uploads/2022/01/italian-pizzeria.png',
+    },
+];
+export const monopoly = [
+    {
+        id: 16,
+        name: 'Manapoli',
+        price: "445|530 som",
+        imageUrl: 'https://momentopizza.com.ua/wp-content/uploads/2020/05/17-copy.png',
+    },
+];
+export const homemade = [
+    {
+        id: 17,
+        name: 'Homemade',
+        price: "500|750 som",
+        imageUrl: 'https://jow.fr/_next/image?url=https%3A%2F%2Fstatic.jow.fr%2F880x880%2Frecipes%2FnuSTxvetkb.png&w=2560&q=100',
+    },
+];
+export const hawaiana = [
+    {
+        id: 18,
+        name: 'Hawayana',
+        price: "480|710 som",
+        imageUrl: 'https://tofuu.getjusto.com/orioneat-prod-resized/j5gFcQgYqneBwvkFk-1200-1200.webp',
     },
 ];
 
@@ -95,28 +167,50 @@ export const italian = [
 //// COLD DRINK /////
 export const pepsi = [
     {
-        id: 10,
+        id: 19,
         name: 'Pepsi',
         price: "100|130 som",
         imageUrl: 'https://www.pepsimax.se/prod/s3fs-public/2022-12/Pepsi%20Reg%2033cl%20Sleek%20Can%20LoRes%20Web.png',
     },
 ];
-
 export const cola = [
     {
-        id: 11,
+        id: 20,
         name: 'Coca Cola',
         price: "100|130 som",
         imageUrl: 'https://www.naughtygrapett.com/wp-content/uploads/2021/09/Coca_colo_can_8oz_12391046-min.png',
     },
 ];
-
 export const fanta = [
     {
-        id: 12,
+        id: 21,
         name: 'Fanta',
         price: "110|140 som",
         imageUrl: 'https://api.e-dostavka.by/UserFiles/images/catalog/Goods/2105/00012105/norm/00012105.n_1.png',
+    },
+];
+export const sprite = [
+    {
+        id: 22,
+        name: 'Sprite',
+        price: "110|140 som",
+        imageUrl: 'https://www.shortysliquor.com.au/media/catalog/product/cache/31c831e646b0e3d5fc6a02c7bbf2f6a4/4/0/40206_2.png',
+    },
+];
+export const sevenUp = [
+    {
+        id: 23,
+        name: '7UP',
+        price: "105|135 som",
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0653/8349/7942/products/7upLemon_Bubbles330ml.png?v=1670595369&width=2048',
+    },
+];
+export const schweppes = [
+    {
+        id: 24,
+        name: 'Schweppes',
+        price: "105|135 som",
+        imageUrl: 'https://winetime.md/wp-content/uploads/2022/05/schweppes_indian_250-1.png',
     },
 ];
 //// COLD DRINK /////
@@ -125,7 +219,7 @@ export const fanta = [
 //// HOT DRINK /////
 export const tea = [
     {
-        id: 13,
+        id: 25,
         name: 'Tea',
         price: "20|25 som",
         imageUrl: 'https://pngimg.com/d/tea_PNG16927.png',
@@ -133,7 +227,7 @@ export const tea = [
 ];
 export const teaMilk = [
     {
-        id: 14,
+        id: 26,
         name: 'Tea Milk',
         price: "25|30 som",
         imageUrl: 'https://www.wushiland-usa.com/product/image/pics/%E7%A2%B3%E5%9F%B9%E7%83%8F%E9%BE%8D%E5%A5%B6%E8%8C%B6.png',
@@ -141,7 +235,7 @@ export const teaMilk = [
 ];
 export const cappuccino = [
     {
-        id: 15,
+        id: 27,
         name: 'Cappuccino',
         price: "110|160 som",
         imageUrl: 'https://media.istockphoto.com/id/942186500/vector/coffee-art-vector-cup-top-view-hot-cappuccino-coffee-white-mug-realistic-illustration.jpg?s=612x612&w=0&k=20&c=osx9Liukb8beGyytMiPZM_MJ-wgOU2tSfymmZWT6nVA=',
@@ -149,7 +243,7 @@ export const cappuccino = [
 ];
 export const americano = [
     {
-        id: 16,
+        id: 28,
         name: 'Americano',
         price: "100|120 som",
         imageUrl: 'https://franko.su/upload/iblock/103/tw30tu5v1g6fooxafoq14i1w3802i3l9.png',
@@ -157,7 +251,7 @@ export const americano = [
 ];
 export const mocha = [
     {
-        id: 17,
+        id: 29,
         name: 'Mocha',
         price: "190|220 som",
         imageUrl: 'https://www.starbucksathome.com/kz/sites/default/files/styles/rdp_banner_image/public/2021-06/10032021_CafeMocha_CS-min.png?itok=6Y-TdzYT',
@@ -165,7 +259,7 @@ export const mocha = [
 ];
 export const latte = [
     {
-        id: 18,
+        id: 30,
         name: 'Latte',
         price: "100|190 som",
         imageUrl: 'https://cdn.shopify.com/s/files/1/0319/0764/3436/products/vanilla-caffe-latte.png?v=1597269391',
@@ -177,7 +271,7 @@ export const latte = [
 //// EASTERN CUISINE //////
 export const baklava = [
     {
-        id: 19,
+        id: 31,
         name: 'Baklava',
         price: "90|125 som",
         imageUrl: 'https://play-lh.googleusercontent.com/FD-_DXtKoh-ROND3H1BzF5lpRWw7r4JAuAsJc7Rq-l11mz4wn83kr-ZZ2h48mvHW-DM',
@@ -185,7 +279,7 @@ export const baklava = [
 ];
 export const shawarma = [
     {
-        id: 20,
+        id: 32,
         name: 'Shawarma',
         price: "130|200 som",
         imageUrl: 'https://www.shawarmabarlow.com/assets/images/Beef-Donair-1000.png',
@@ -193,7 +287,7 @@ export const shawarma = [
 ];
 export const shashlik = [
     {
-        id: 21,
+        id: 33,
         name: 'Shashlik',
         price: "300|450 som",
         imageUrl: 'https://indelika.com/wp-content/uploads/2020/10/recipe-4.png',
@@ -201,7 +295,7 @@ export const shashlik = [
 ];
 export const kofta = [
     {
-        id: 22,
+        id: 34,
         name: 'Kofta',
         price: "200|270 som",
         imageUrl: 'https://static.wixstatic.com/media/d32071_44fa4578b54449f6a285289d9a8a6811~mv2.png/v1/fill/w_611,h_408,al_c/d32071_44fa4578b54449f6a285289d9a8a6811~mv2.png',
@@ -209,7 +303,7 @@ export const kofta = [
 ];
 export const hummus = [
     {
-        id: 23,
+        id: 35,
         name: 'Hummus',
         price: "215|280 som",
         imageUrl: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/12/19/0/FNM_010114-Homemade-Hummus-Recipe_s4x3.jpg.rend.hgtvcom.616.462.suffix/1389218464364.jpeg',
@@ -217,7 +311,7 @@ export const hummus = [
 ];
 export const kanafeh = [
     {
-        id: 23,
+        id: 36,
         name: 'Kanafeh',
         price: "220|260 som",
         imageUrl: 'https://knafehhouse.com/wp-content/uploads/2020/07/Knafeh-FG1.png',
