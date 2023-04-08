5497536e4184dba46c3f20e9be6e661ae076254c
Aidar06 <kabylbekovajdar94@gmail.com>
2023-04-08 23:59:29 +0600

    fixSet

--- src/pages/Settings/settings.js
+++ src/pages/Settings/settings.js
@@ -10,6 +10,8 @@ import {MdNotifications} from "react-icons/md";
 import {BiLogOut} from "react-icons/bi";
 import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
+import {elKart, mbank, visa} from "../../components/FakeBackend/FakeBackend";
+import {AiFillFacebook} from "react-icons/ai";
 
 const Settings = () => {
     const navigate = useNavigate()
@@ -27,11 +29,9 @@ const Settings = () => {
             name = el.name
         }
     })
-    return (<div id="settings">
-import {elKart, mbank, visa} from "../../components/FakeBackend/FakeBackend";
-import {AiFillFacebook} from "react-icons/ai";
 
-const Settings = () => {
+
+
     const [payment, setPayment] = useState(false)
     const [social, setSocial] = useState(false)
 
@@ -72,114 +72,117 @@ const Settings = () => {
                     </div>
 
                     <div onClick={()=> {logOut(); navigate('/signIn')}} className="settings--login">
-                    <div style={{
-                        display: payment === true ? "block" : "none",
-                    }} className="settings--share">
+                        <div style={{
+                            display: payment === true ? "block" : "none",
+                        }} className="settings--share">
 
-                        <div className="settings--share__text">
-                            <h2>Payment</h2>
-                            <button onClick={() => {
-                                setPayment(false)
-                            }} className="settings--share__text--icon">
-                                &times;
-                            </button>
-                        </div>
-
-                        <div className="settings--share__payment">
-                            <div className="settings--share__payment--elKart">
-                                {
-                                    elKart.map(el => (
-                                        <img src={el.imageUrl} alt="img"/>
-                                    ))
-                                }
-                            </div>
-                            <div className="settings--share__payment--mBank">
-                                {
-                                    mbank.map(el => (
-                                        <img src={el.imageUrl} alt="img"/>
-                                    ))
-                                }
-                            </div>
-                            <div className="settings--share__payment--visa">
-                                {
-                                    visa.map(el => (
-                                        <img src={el.imageUrl} alt="img"/>
-                                    ))
-                                }
-                            </div>
-                        </div>
-                    </div>
-
-                    <div style={{
-                        background: payment === true ? "rgba(133,133,133,0.62)" : "",
-                    }} className="settings--login">
-                        <TbLock className="settings--login__icon"/>
-                        <h3>Login & Password</h3>
-                    </div>
-
-                    <div style={{
-                        background: payment === true ? "rgba(133,133,133,0.62)" : "",
-                    }} onClick={() => setSocial(true)}
-                         className="settings--friend">
-                        <FaUserFriends className="settings--friend__icon"/>
-                        <h3>Invite a Friend</h3>
-                    </div>
-
-                    <div style={{
-                        display: social === true ? "block" : "none",
-                    }} className="settings--invites">
-
-                        <div className="settings--invites__text">
-                            <h3>Share Friends</h3>
-                            <button onClick={() => {
-                                setSocial(false)
-                            }} className="settings--invites__text--icon">
-                                &times;
-                            </button>
-                        </div>
-
-                        <div className="settings--invites__social">
-                            <div className="settings--invites__social--whatsapp">
-                                <a href="https://b24-pememt.bitrix24.site/crm_form_p2gyo/">
-                                    <BsWhatsapp
-                                        className="settings--invites__social--whatsapp__icon"
-                                    />
-                                </a>
+                            <div className="settings--share__text">
+                                <h2>Payment</h2>
+                                <button onClick={() => {
+                                    setPayment(false)
+                                }} className="settings--share__text--icon">
+                                    &times;
+                                </button>
                             </div>
 
-                            <div className="settings--invites__social--instagram">
-                                <a href="https://www.instagram.com/motion_web/">
-                                    <BsInstagram
-                                        className="settings--invites__social--instagram__icon"
-                                    />
-                                </a>
-                            </div>
-                            <div className="settings--invites__social--facebook">
-                                <a style={{display: "flex", alignItems: "center"}}
-                                   href="https://www.instagram.com/motion_web/">
-                                    <AiFillFacebook
-                                        className="settings--invites__social--facebook__icon"
-                                    />
-                                </a>
+                            <div className="settings--share__payment">
+                                <div className="settings--share__payment--elKart">
+                                    {
+                                        elKart.map(el => (
+                                            <img src={el.imageUrl} alt="img"/>
+                                        ))
+                                    }
+                                </div>
+                                <div className="settings--share__payment--mBank">
+                                    {
+                                        mbank.map(el => (
+                                            <img src={el.imageUrl} alt="img"/>
+                                        ))
+                                    }
+                                </div>
+                                <div className="settings--share__payment--visa">
+                                    {
+                                        visa.map(el => (
+                                            <img src={el.imageUrl} alt="img"/>
+                                        ))
+                                    }
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div style={{
-                        background: payment === true ? "rgba(133,133,133,0.62)" : "",
-                    }} className="settings--notification">
-                        <MdNotifications className="settings--notification__icon"/>
-                        <h3>Notification</h3>
-                    </div>
+                        <div style={{
+                            background: payment === true ? "rgba(133,133,133,0.62)" : "",
+                        }} className="settings--login">
+                            <TbLock className="settings--login__icon"/>
+                            <h3>Login & Password</h3>
+                        </div>
 
-                    <div onClick={()=> {logOut(); navigate('/')}} className="settings--logOut">
-                        <BiLogOut className="settings--notification__icon"/>
-                        <h3>Log Out</h3>
-                    </div>
+                        <div style={{
+                            background: payment === true ? "rgba(133,133,133,0.62)" : "",
+                        }} onClick={() => setSocial(true)}
+                             className="settings--friend">
+                            <FaUserFriends className="settings--friend__icon"/>
+                            <h3>Invite a Friend</h3>
+                        </div>
 
+                        <div style={{
+                            display: social === true ? "block" : "none",
+                        }} className="settings--invites">
+
+                            <div className="settings--invites__text">
+                                <h3>Share Friends</h3>
+                                <button onClick={() => {
+                                    setSocial(false)
+                                }} className="settings--invites__text--icon">
+                                    &times;
+                                </button>
+                            </div>
+
+                            <div className="settings--invites__social">
+                                <div className="settings--invites__social--whatsapp">
+                                    <a href="https://b24-pememt.bitrix24.site/crm_form_p2gyo/">
+                                        <BsWhatsapp
+                                            className="settings--invites__social--whatsapp__icon"
+                                        />
+                                    </a>
+                                </div>
+
+                                <div className="settings--invites__social--instagram">
+                                    <a href="https://www.instagram.com/motion_web/">
+                                        <BsInstagram
+                                            className="settings--invites__social--instagram__icon"
+                                        />
+                                    </a>
+                                </div>
+                                <div className="settings--invites__social--facebook">
+                                    <a style={{display: "flex", alignItems: "center"}}
+                                       href="https://www.instagram.com/motion_web/">
+                                        <AiFillFacebook
+                                            className="settings--invites__social--facebook__icon"
+                                        />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style={{
+                            background: payment === true ? "rgba(133,133,133,0.62)" : "",
+                        }} className="settings--notification">
+                            <MdNotifications className="settings--notification__icon"/>
+                            <h3>Notification</h3>
+                        </div>
+
+                        <div onClick={()=> {logOut(); navigate('/')}} className="settings--logOut">
+                            <BiLogOut className="settings--notification__icon"/>
+                            <h3>Log Out</h3>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>);
+        </div>
+       );
+
 };
 
 export default Settings;
