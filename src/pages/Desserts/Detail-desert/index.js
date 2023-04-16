df0c24abcdb3e626107204b5eec4a4323824df28
cerbent <kalykovadilet2004@gmail.com>
2023-04-16 18:52:22 +0600

    finishADap

--- src/pages/Desserts/Detail-desert/index.js
+++ src/pages/Desserts/Detail-desert/index.js
@@ -8,7 +8,7 @@ import DetailBtn from "../../detail-page/DetailBtn";
 const DetailDesert = () => {
     const dispatch = useDispatch()
     const {basket} = useSelector(state => state)
-    const {name,imageUrl,price} = basket
+    const {name,imageUrl,price,quantity} = basket
     const aDDOrder = (el) => {
         dispatch({type: "ADD_ORDER", payload: el})
     }
@@ -21,9 +21,13 @@ const DetailDesert = () => {
                     <div className='detail--img'>
                         <img src={imageUrl} alt=""/>
                         <p>{name}</p>
-                        <h2>{price}</h2>
+                        <h2>{price}som</h2>
                     </div>
-                    <DetailBtn/>
+                    <div className="flex items-center justify-center">
+                        <button>-</button>
+                        <h1 className="text-3xl  px-3">{quantity}</h1>
+                        <button>+</button>
+                    </div>
                     <NavLink to={"/my-order"}>
                     <button onClick={() => aDDOrder(basket)} className='detail--btn my-6'>ADD TO CART</button>
                     </NavLink>
--- src/pages/Eastern-Cuisine/Eastern/easternDetail.js
+++ src/pages/Eastern-Cuisine/Eastern/easternDetail.js
@@ -7,7 +7,7 @@ import DetailBtn from "../../detail-page/DetailBtn";
 const EasternDetail = () => {
     const {basket} = useSelector(state => state)
     const dispatch = useDispatch()
-    const {name,imageUrl,price} = basket
+    const {name,imageUrl,price,quantity} = basket
 
     const addOrder = (el) =>{
         dispatch({type: "ADD_ORDER",payload: el})
@@ -20,9 +20,13 @@ const EasternDetail = () => {
                     <div className='detail--img'>
                         <img className='mx-[30%]' src={imageUrl} alt="img"/>
                         <p>{name}</p>
-                        <h2>{price}</h2>
+                        <h2>{price}som</h2>
                     </div>
-                    <DetailBtn/>
+                    <div className="flex items-center justify-center">
+                        <button>-</button>
+                        <h1 className="text-3xl  px-3">{quantity}</h1>
+                        <button>+</button>
+                    </div>
                     <NavLink to={"/my-order"}>
                         <button onClick={() => addOrder(basket)} className='detail--btn my-6'>ADD TO CART</button>
                     </NavLink>
